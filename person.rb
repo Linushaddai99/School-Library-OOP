4352766275526d43c61588a121de913ef1389c36
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-02-01 13:10:54 +0100

    Add class decorators

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
-  attr_reader :id
+require './nameable'
+
+class Person < Nameable
+  attr_reader :id, :correct_name
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000),
           @name = name,
           @age = age,
@@ -11,7 +14,7 @@ class Person
 
   def can_use_services?
     @age >= 18 || parent_permission
-  end
+  en
 
   private
 
